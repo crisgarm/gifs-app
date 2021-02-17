e8fc1c03bdd3baa5fe3241fb2d6cd7e4f84c723f
crisgarm <crisgarm9@gmail.com>
2021-02-17 13:24:16 +0100

    Create gifs module, gifs page component and his sons, search and results components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, SharedModule, GifsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
